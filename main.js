d327fa2f794b7153fa735fd694b796fa4b4168d8
zalesakz <zdenda.zalesak@gmail.com>
2023-11-29 21:23:44 +0100

    doucko2

---
+++ main.js
@@ -0,0 +1,16 @@
+const seznam = [
+    {Jmeno: "Zdeněk", Prijmeni: "Zálešák", Rocnik: "2."},
+    {Jmeno: "Jan", Prijmeni: "Novák", Rocnik: "1."}
+];
+
+function VypisDoTabulky()
+{
+    let table = document.getElementById("studentTable");
+         
+}
+
+function Pridat()
+{
+    VypisDoTabulky();
+
+}
