a696840d52bbf71b4b239cb0ca60f555ffcc76f2
ZalesakZ <150954612+ZalesakZ@users.noreply.github.com>
2023-11-30 01:51:20 +0100

    zkouseni doma

--- main.js
+++ main.js
@@ -5,12 +5,49 @@ const seznam = [
 
 function VypisDoTabulky()
 {
-    let table = document.getElementById("studentTable");
-         
+    //let seznam = "";
+
+    for(let i = 0; i < seznam.length; i++)
+    {
+        let table = document.getElementById("studentTable");
+        let row = table.insertRow(0);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        cell1.innerHTML = seznam[i].Jmeno;
+        cell2.innerHTML = seznam[i].Prijmeni; 
+        cell3.innerHTML = seznam[i].Rocnik;   
+        
+        //seznam[i] += document.innerHTML;
+        
+    }  
+
 }
 
 function Pridat()
 {
-    VypisDoTabulky();
+    let jmeno = document.getElementById("name").value;
+    let prijmeni = document.getElementById("surname").value;
+    let rocnik = document.getElementById("rocnik").value;
 
+    if((jmeno != "") && (isNaN(jmeno))) // && (jmeno.value < 0) && (jmeno.value > 10)) 
+        if((prijmeni != "") && (isNaN(prijmeni)))
+            if((rocnik != "") && (rocnik < 1) && (rocnik > 4))
+
+
+        {
+             
+        } 
+        else
+        {
+            alert("jsi kkt") 
+        }
+    
+    VypisDoTabulky(); 
+    seznam.push({Jmeno: jmeno, Prijmeni: prijmeni, Rocnik: rocnik})
+
+    
 }
+
+
+
